7b6d2a62650ef0319827253bdd1438b89f43e88f
hamidwise1 <hrawebdev@gmail.com>
2025-07-20 10:30:18 +0330

    update test table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,16 @@
-import { mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core';
+import {
+ mysqlTable,
+ serial,
+ varchar,
+ int,
+ timestamp,
+} from 'drizzle-orm/mysql-core';
 
 export const testTable = mysqlTable('test', {
- id: serial('id').primaryKey(),
- other: varchar('other', { length: 10 }),
+ id: serial().primaryKey(),
+ firstName: varchar({ length: 100 }).notNull(),
+ lastName: varchar({ length: 100 }).notNull(),
+ age: int().notNull(),
+ email: varchar({ length: 100 }),
+ createdAt: timestamp().defaultNow(),
 });
