e0edb2c135fc3245ec0a4295cea0178d29207639
hamidwise1 <hrawebdev@gmail.com>
2025-07-26 11:01:43 +0330

    update zod catch error

--- src/utils/expressCatchErrors.ts
+++ src/utils/expressCatchErrors.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler } from 'express';
 import { status } from 'http-status';
 import { getResponse } from './getResponse.ts';
+import { ZodError } from 'zod';
 
 export const expressCatchErrors: ErrorRequestHandler = (
  err,
@@ -8,10 +9,22 @@ export const expressCatchErrors: ErrorRequestHandler = (
  res,
  _next
 ) => {
- console.log(err.message);
+ const resStatus = 'failed';
+ // handle zod validation error
+ if (err instanceof ZodError) {
+  res.status(status.BAD_REQUEST).json(
+   getResponse({
+    status: resStatus,
+    message: err.message,
+   })
+  );
+  return;
+ }
+ //  what else should be handled here?
+ //
  res.status(status.INTERNAL_SERVER_ERROR).json(
   getResponse({
-   status: 'failed',
+   status: resStatus,
    message: err.message,
   })
  );
