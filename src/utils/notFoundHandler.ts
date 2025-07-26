4a15e35ce11263070ab1aa23a14a136aac966abc
hamidwise1 <hrawebdev@gmail.com>
2025-07-26 11:25:42 +0330

    update not found handler

--- src/utils/notFoundHandler.ts
+++ src/utils/notFoundHandler.ts
@@ -1,12 +1,7 @@
-import { Request, Response } from 'express';
-import { getResponse } from './getResponse.ts';
-import { status } from 'http-status';
+import { NotFound } from './NotFound.ts';
 
-export function notFoundHandler(_: Request, res: Response) {
- res.status(status.NOT_FOUND).json(
-  getResponse({
-   status: 'failed',
-   message: 'API endpoint not found. Please check the URL and try again.',
-  })
+export function notFoundHandler() {
+ throw new NotFound(
+  'API endpoint not found. Please check the URL and try again.'
  );
 }
